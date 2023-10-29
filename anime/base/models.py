f45850e09b643901ecc0802641e462f724b85cf1
abdullah <abdullah@gmail.com>
2023-10-30 02:18:02 +0600

    First commit

---
+++ anime/base/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.db.models.deletion import CASCADE
+from django.contrib.auth.models import AbstractUser
+
+
+
+class User(AbstractUser):
+    name=models.CharField(max_length=200, null=True)
+    email=models.EmailField(unique=True,null=True)
+    bio= models.TextField(null=True)
+
+    avatar= models.ImageField(null=True, default='avatar.svg')
+    
+    premium = models.BooleanField(default=False)
+    gender = models.CharField(max_length=20, null=True)
+    newslatter= models.BooleanField(default=False)
+    
+
+    USERNAME_FIELD= 'email'
+    REQUIRED_FIELDS= ['username']
